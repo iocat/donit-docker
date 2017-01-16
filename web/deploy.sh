4ff16005669ee864d85995f8ade08016df47f362
Thanh Ngo <felix.infinite@gmail.com>
2017-01-16 01:38:26 -0500

    edit deploy scripts: work now

--- web/deploy.sh
+++ web/deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 if [ -d "build" ]; then
 	docker run -d \
-		-v $(pwd)/build:/build \
+        -p 9000:9000 \
+		-v $(pwd)/build:/public \
 		--name=donit-web iocat/donit-web
 else 
 	printf "build directory does not exist"
