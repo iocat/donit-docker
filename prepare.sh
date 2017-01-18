2afc6d5b280f0626f51d98b2d2ec70427b4e0d99
Thanh Ngo <felix.infinite@gmail.com>
2017-01-18 05:50:45 +0000

    add prepare and deploy script

---
+++ prepare.sh
@@ -0,0 +1,3 @@
+mkdir -r /var/lib/cockroach/data1
+git clone https://github.com/iocat/donitui.git -b gh-pages /var/www/www.donit.xyz
+git clone https://github.com/iocat/donitui.git -b gh-pages /var/www/test.donit.xyz
